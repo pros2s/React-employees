61d643af7972d9636a0bcd70c24c125f8de28ddc
Alexandr <caiiiia_podoplelov@mail.ru>
2022-04-04 04:44:16 +0300

    states and actions(onClick)

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,28 +1,55 @@
+import { Component } from 'react';
 import './employees-list-item.css';
 
-const EmployeesListItem = ({name, surname, salary, increase}) => {
-  const classList = increase ?
-    "list-group-item d-flex justify-content-between increase" :
-    "list-group-item d-flex justify-content-between";
-    
-  return (
-    <li className={classList}>
-      <span className="list-group-item-label">{name} {surname}</span>
-      <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
-      <div className='d-flex justify-content-center align-items-center'>
-        <button type="button"
-          className="btn-cookie btn-sm ">
-          <i className="fas fa-cookie"></i>
-        </button>
+class EmployeesListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      increase: false,
+      like: false
+    };
+  }
 
-        <button type="button"
-          className="btn-trash btn-sm ">
-          <i className="fas fa-trash"></i>
-        </button>
-        <i className="fas fa-star"></i>
-      </div>
-    </li>
-  )
+  cookieColor = () => {
+    this.setState(({increase}) => ({
+      increase: !increase
+    }));
+  }
+
+  likeStar = () => {
+    this.setState(({like}) => ({
+      like: !like
+    }));
+  }
+
+  render() {
+    const {name, surname, salary} = this.props;
+    const {increase, like} = this.state;
+
+    let classList = 'list-group-item d-flex justify-content-between';
+    if (increase) classList += ' increase';
+    if (like) classList += ' like';
+
+    return (
+      <li className={classList}>
+        <span className='list-group-item-label' onClick={this.likeStar}>{name} {surname}</span>
+        <input type='text' className='list-group-item-input' defaultValue={salary + '$'}/>
+        <div className='d-flex justify-content-center align-items-center'>
+          <button type='button'
+            className='btn-cookie btn-sm '
+            onClick={this.cookieColor}>
+            <i className='fas fa-cookie'></i>
+          </button>
+
+          <button type='button'
+            className='btn-trash btn-sm '>
+            <i className='fas fa-trash'></i>
+          </button>
+          <i className='fas fa-star'></i>
+        </div>
+      </li>
+    )
+  }
 }
 
 export default EmployeesListItem;
